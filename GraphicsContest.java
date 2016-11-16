ba7bdf6271662272d8901c8844e1ba8a207abf02
S <s>
2016-11-16 14:15:50 -0800

    {"types":"C","runs":126}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -25,8 +25,8 @@ public class GraphicsContest extends GraphicsProgram {
 			double r = rgen.nextDouble(20,50);
 			double vx = rgen.nextDouble(1,5);
 			double vy = rgen.nextDouble(1,5);
-			double x = rgen.nextDouble(getWidth() - r/2, getHeight() - r/2);
-			double y = rgen.nextDouble(getWidth() - r/2, getHeight() - r/2);
+			double x = rgen.nextDouble(0 + r/2, getWidth() - r/2);
+			double y = rgen.nextDouble(0 + r/2, getHeight() - r/2);
 			GOval ball = new GOval (x - r/2, y - r/2, r*2, r*2);
 			ball.setFilled(true);
 			ball.setColor(rgen.nextColor());
