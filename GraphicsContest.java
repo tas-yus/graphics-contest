62aa58d58332fe960f01cb2581ef1e9a57ae932d
S <s>
2016-11-17 11:11:38 -0800

    {"types":"C","runs":403}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -371,7 +371,7 @@ public class GraphicsContest extends GraphicsProgram {
 			Color color = new Color (rgen.nextInt(0,128), rgen.nextInt(0,255),255);
 			return color;
 		} else if (chosenMixedColor == PURPLE) {
-			Color color = new Color (255, rgen.nextInt(128,255),0);
+			Color color = new Color (rgen.nextInt(0,255), 0,rgen.nextInt(127,255));
 			return color;
 		} else return null;
 	}
