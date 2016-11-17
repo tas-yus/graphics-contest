cdc76d7b085839827e30c5cdaf23c532cb99f4b7
S <s>
2016-11-16 22:58:29 -0800

    {"types":"C","runs":237}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -14,11 +14,12 @@ import java.awt.event.*;
 public class GraphicsContest extends GraphicsProgram {
 
 	private RandomGenerator rgen = RandomGenerator.getInstance();
-	private static final int WIDTH = 750;
+	private static final int WIDTH = 700;
     private static final int HEIGHT = 700;
     private static final int PAUSE = 10;
     private static final int s = 5;
     private Color newColor = new Color (255, 128, 0);
+    private boolean draw = false;
     
 	public void run() {
 		this.resize(WIDTH,HEIGHT);
@@ -27,7 +28,12 @@ public class GraphicsContest extends GraphicsProgram {
 	}
 	
 	public void mouseClicked(MouseEvent e) {
-		newColor = new Color (255,rgen.nextInt(128,255),0);
+		if (draw == false) {
+			draw = true;
+		}
+		if (draw == true) {
+			draw = false;
+		}
 	}
 	
 	public void mouseMoved(MouseEvent e) {
@@ -43,38 +49,40 @@ public class GraphicsContest extends GraphicsProgram {
 	}
 	private void addAll(int s, double x, double y) {
 		newColor = new Color (255,rgen.nextInt(128,255),0);
-		GOval pixel1 = new GOval (getWidth()/2 - x - s/2, getHeight()/2 - y - s/2, s, s);
-		pixel1.setFilled(true);
-		pixel1.setColor(newColor);
-		GOval pixel2 = new GOval (getWidth()/2 - y - s/2, getHeight()/2 - x - s/2, s, s);
-		pixel2.setFilled(true);
-		pixel2.setColor(newColor);
-		GOval pixel3 = new GOval (getWidth()/2 + x - s/2, getHeight()/2 + y - s/2, s, s);
-		pixel3.setFilled(true);
-		pixel3.setColor(newColor);
-		GOval pixel4 = new GOval (getWidth()/2 + y - s/2, getHeight()/2 + x - s/2, s, s);
-		pixel4.setFilled(true);
-		pixel4.setColor(newColor);
-		GOval pixel5 = new GOval (getWidth()/2 - x - s/2, getHeight()/2 + y - s/2, s, s);
-		pixel5.setFilled(true);
-		pixel5.setColor(newColor);
-		GOval pixel6 = new GOval (getWidth()/2 - y - s/2, getHeight()/2 + x - s/2, s, s);
-		pixel6.setFilled(true);
-		pixel6.setColor(newColor);
-		GOval pixel7 = new GOval (getWidth()/2 + x - s/2, getHeight()/2 - y - s/2, s, s);
-		pixel7.setFilled(true);
-		pixel7.setColor(newColor);
-		GOval pixel8 = new GOval (getWidth()/2 + y - s/2, getHeight()/2 - x - s/2, s, s);
-		pixel8.setFilled(true);
-		pixel8.setColor(newColor);
-		add(pixel1);
-		add(pixel2);
-		add(pixel3);
-		add(pixel4);
-		add(pixel5);
-		add(pixel6);
-		add(pixel7);
-		add(pixel8);
+		if(draw = true) {
+			GOval pixel1 = new GOval (getWidth()/2 - x - s/2, getHeight()/2 - y - s/2, s, s);
+			pixel1.setFilled(true);
+			pixel1.setColor(newColor);
+			GOval pixel2 = new GOval (getWidth()/2 - y - s/2, getHeight()/2 - x - s/2, s, s);
+			pixel2.setFilled(true);
+			pixel2.setColor(newColor);
+			GOval pixel3 = new GOval (getWidth()/2 + x - s/2, getHeight()/2 + y - s/2, s, s);
+			pixel3.setFilled(true);
+			pixel3.setColor(newColor);
+			GOval pixel4 = new GOval (getWidth()/2 + y - s/2, getHeight()/2 + x - s/2, s, s);
+			pixel4.setFilled(true);
+			pixel4.setColor(newColor);
+			GOval pixel5 = new GOval (getWidth()/2 - x - s/2, getHeight()/2 + y - s/2, s, s);
+			pixel5.setFilled(true);
+			pixel5.setColor(newColor);
+			GOval pixel6 = new GOval (getWidth()/2 - y - s/2, getHeight()/2 + x - s/2, s, s);
+			pixel6.setFilled(true);
+			pixel6.setColor(newColor);
+			GOval pixel7 = new GOval (getWidth()/2 + x - s/2, getHeight()/2 - y - s/2, s, s);
+			pixel7.setFilled(true);
+			pixel7.setColor(newColor);
+			GOval pixel8 = new GOval (getWidth()/2 + y - s/2, getHeight()/2 - x - s/2, s, s);
+			pixel8.setFilled(true);
+			pixel8.setColor(newColor);
+			add(pixel1);
+			add(pixel2);
+			add(pixel3);
+			add(pixel4);
+			add(pixel5);
+			add(pixel6);
+			add(pixel7);
+			add(pixel8);
+		}
 	}
 	
 
