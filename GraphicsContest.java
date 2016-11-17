f2a88ebffd646df9d6a6763abc9e9b25d5dc2558
S <s>
2016-11-17 11:39:46 -0800

    {"types":"C","runs":429}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -67,9 +67,7 @@ public class GraphicsContest extends GraphicsProgram {
 		setUpColors();
 		setUpIcons();
 		setUpColorChoice();
-		if (auto == true) {
-			autoDraw();
-		}
+		autoDraw();
 	}
 
 	private void autoDraw() {
