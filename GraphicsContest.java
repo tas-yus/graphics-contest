34fea598d61764b8944a9ebb2fe1b160171284eb
S <s>
2016-11-17 14:21:48 -0800

    {"types":"C","runs":496}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -366,8 +366,8 @@ public class GraphicsContest extends GraphicsProgram {
 			double x = e.getX();
 			double y = e.getY();
 			if (y > ICON_HEIGHT + s) {
-				x = x - WIDTH/2 - s;
-				y = y - (HEIGHT/2 + ICON_HEIGHT) - s;
+				x = x - WIDTH/2 - s/2;
+				y = y - (HEIGHT/2 + ICON_HEIGHT) - s/2;
 				setUpBall(x, y);
 			}
 		}
