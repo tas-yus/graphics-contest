42c14d0186fa6282c527c28553896328d14727ff
S <s>
2016-11-16 22:04:33 -0800

    {"types":"C","runs":218}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -17,7 +17,7 @@ public class GraphicsContest extends GraphicsProgram {
 	private static final int WIDTH = 700;
     private static final int HEIGHT = 700;
     private static final int PAUSE = 10;
-    private static final int s = 10;
+    private static final int s = 5;
     private Color newColor = new Color (255, 128, 0);
     
 	public void run() {
@@ -45,35 +45,27 @@ public class GraphicsContest extends GraphicsProgram {
 		GOval pixel1 = new GOval (getWidth()/2 - x - s/2, getHeight()/2 - y - s/2, s, s);
 		pixel1.setFilled(true);
 		pixel1.setColor(newColor);
-		pixel1.setFillColor(Color.WHITE);
 		GOval pixel2 = new GOval (getWidth()/2 - y - s/2, getHeight()/2 - x - s/2, s, s);
 		pixel2.setFilled(true);
 		pixel2.setColor(newColor);
-		pixel2.setFillColor(Color.WHITE);
 		GOval pixel3 = new GOval (getWidth()/2 + x - s/2, getHeight()/2 + y - s/2, s, s);
 		pixel3.setFilled(true);
 		pixel3.setColor(newColor);
-		pixel3.setFillColor(Color.WHITE);
 		GOval pixel4 = new GOval (getWidth()/2 + y - s/2, getHeight()/2 + x - s/2, s, s);
 		pixel4.setFilled(true);
 		pixel4.setColor(newColor);
-		pixel4.setFillColor(Color.WHITE);
 		GOval pixel5 = new GOval (getWidth()/2 - x - s/2, getHeight()/2 + y - s/2, s, s);
 		pixel5.setFilled(true);
 		pixel5.setColor(newColor);
-		pixel5.setFillColor(Color.WHITE);
 		GOval pixel6 = new GOval (getWidth()/2 - y - s/2, getHeight()/2 + x - s/2, s, s);
 		pixel6.setFilled(true);
 		pixel6.setColor(newColor);
-		pixel6.setFillColor(Color.WHITE);
 		GOval pixel7 = new GOval (getWidth()/2 + x - s/2, getHeight()/2 - y - s/2, s, s);
 		pixel7.setFilled(true);
 		pixel7.setColor(newColor);
-		pixel7.setFillColor(Color.WHITE);
 		GOval pixel8 = new GOval (getWidth()/2 + y - s/2, getHeight()/2 - x - s/2, s, s);
 		pixel8.setFilled(true);
 		pixel8.setColor(newColor);
-		pixel8.setFillColor(Color.WHITE);
 		add(pixel1);
 		add(pixel2);
 		add(pixel3);
