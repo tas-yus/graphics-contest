e42d336ba044159c6412b40bacedfa3e0ad08c6b
S <s>
2016-11-16 17:53:58 -0800

    {"types":"C","runs":179}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -14,25 +14,26 @@ import java.awt.event.*;
 public class GraphicsContest extends GraphicsProgram {
 
 	private RandomGenerator rgen = RandomGenerator.getInstance();
-	
+	private static final int WIDTH = 400;
+    private static final int HEIGHT = 400;
+    private static final int PAUSE = 10;
+    
 	public void run() {
+		this.resize(WIDTH,HEIGHT);
+        pause(PAUSE);
 		setUpBall();
 
 	}
 
 	private void setUpBall() {
-		while (true) {
-			int s = 10;
-			int x = rgen.nextInt(0 + 2*s, getWidth() - 2*s);
-			int y = rgen.nextInt(0 + 2*s, getHeight() - 2*s);
-			GRect pixel = new GRect (x - s, y - s, s*2, s*2);
-			pixel.setFilled(true);
-			pixel.setColor(rgen.nextColor());
-			if (getElementAt(x,y) != null){
-	            remove(getElementAt(x,y));
-			}
-			add(pixel);
-			pause(1);
+		int s = 10;
+		int x = rgen.nextInt(0 + 2*s, getWidth() - 2*s);
+		int y = rgen.nextInt(0 + 2*s, getHeight() - 2*s);
+		GRect pixel = new GRect (x - s, y - s, s*2, s*2);
+		pixel.setFilled(true);
+		pixel.setColor(rgen.nextColor());
+		if (getElementAt(x,y) != null){
+			remove(getElementAt(x,y));
 		}
 	}
 
