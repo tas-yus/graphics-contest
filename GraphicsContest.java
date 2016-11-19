d7127897d7e268e74d31a1585281641a991bc63e
S <s>
2016-11-19 14:12:50 +0700

    {"types":"C","runs":162}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -42,7 +42,7 @@ public class GraphicsContest extends GraphicsProgram {
 	private int s = BRUSH_SIZE;
 	private int speed = DELAY;
 	private int speedLevel = 1;
-	private int symmetry = 4;
+	private int symmetry = 5;
 	private GRect colorTray;
 	private GRect icon1;
 	private GRect icon2;
