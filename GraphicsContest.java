cd5b92d9155de72cff907dc08fab4a548e2c6ece
S <s>
2016-11-18 18:47:54 -0800

    {"types":"C","runs":724}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -618,9 +618,6 @@ public class GraphicsContest extends GraphicsProgram {
 		if (auto == true) {
 			newColor = mixColor(chosenMixedColor);
 		}
-		if (choose == true) {
-			newColor = yourChosenColor;
-		}
 		GOval pixel1 = new GOval (getWidth()/2 - x - s/2, getHeight()/2 + ICON_HEIGHT/2 - y - s/2, s, s);
 		pixel1.setFilled(true);
 		pixel1.setColor(newColor);
