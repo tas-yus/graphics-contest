08cce55a6322e3b3cba15312e75ca7bd75b45982
S <s>
2016-11-20 22:12:12 +0700

    {"types":"C","runs":387}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -289,12 +289,12 @@ public class GraphicsContest extends GraphicsProgram {
 		rotationalArray[0][1] = B;
 		rotationalArray[1][0] = -B;
 		rotationalArray[1][1] = A;
+		slope = new double[plane];
 		double y = getHeight() - (getHeight()/2 + ICON_HEIGHT/2);
 		for (int n = 0; n < fold; n++) {
 			GLine reflectionLine = new GLine (getWidth()/2 + y*(powMatrix(rotationalArray, n)[0][1]), getHeight()/2 + ICON_HEIGHT/2 + y*(powMatrix(rotationalArray, n)[1][1]),
 					getWidth()/2 - y*(powMatrix(rotationalArray, n)[0][1]), getHeight()/2 + ICON_HEIGHT/2 - y*(powMatrix(rotationalArray, n)[1][1]));
 			add(reflectionLine);
-			slope = new double[plane];
 			slope[n] = getSlope(reflectionLine);
 			if (n == 1) {
 				double f = (reflectionLine.getEndPoint()).getX();
