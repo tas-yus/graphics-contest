78e8e02bcb07b480882fb97c5a04bea645323942
S <s>
2016-11-20 22:22:26 +0700

    {"types":"C","runs":406}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -45,7 +45,7 @@ public class GraphicsContest extends GraphicsProgram {
 	private int speed = DELAY;
 	private int speedLevel = 1;
 	private int symmetry = 12;
-	private int plane = 5;
+	private int plane = 1;
 	private GRect colorTray;
 	private GRect icon1;
 	private GRect icon2;
@@ -733,6 +733,8 @@ public class GraphicsContest extends GraphicsProgram {
 		if (reflection == true) {
 			coordinate[1][0] = x;
 			coordinate[0][1] = y;
+			coordinate[2][0] = -x;
+			coordinate[0][2] = y;
 			for (int n = 0; n < fold; n++) {
 				double[][] reflectionArray = new double[2][2];
 				double m = slope[n];
@@ -742,7 +744,6 @@ public class GraphicsContest extends GraphicsProgram {
 				reflectionArray[0][1] = B;
 				reflectionArray[1][0] = B;
 				reflectionArray[1][1] = -A;
-				
 				for (int i = 1; i <= (int) Math.pow(2, n); i++) {
 					coordinate[(int) (Math.pow(2, n) + i)][0] = coordinate[i][0]*(reflectionArray[0][0]) + coordinate[0][i]*(reflectionArray[0][1]);
 					coordinate[0][(int) (Math.pow(2, n) + i)] = coordinate[i][0]*(reflectionArray[1][0]) + coordinate[0][i]*(reflectionArray[1][1]);
