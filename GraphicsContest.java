893cc80d8f0b975c792f4826c8d13a2fa0264217
S <s>
2016-11-20 13:16:31 +0700

    {"types":"C","runs":297}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -290,7 +290,7 @@ public class GraphicsContest extends GraphicsProgram {
 		for (int n = 0; n < fold; n++) {
 			line1 = new GLine (getWidth()/2 + y*(powMatrix(rotationalArray, n)[0][1]), getHeight()/2 + ICON_HEIGHT/2 + y*(powMatrix(rotationalArray, n)[1][1]),
 					getWidth()/2 - y*(powMatrix(rotationalArray, n)[0][1]), getHeight()/2 + ICON_HEIGHT/2 - y*(powMatrix(rotationalArray, n)[1][1]));
-			add(line1);
+			
 		}
 		
 	}
