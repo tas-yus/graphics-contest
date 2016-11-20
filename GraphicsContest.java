eaca43d99e99bcf91620112883a82d6dcb87619c
S <s>
2016-11-20 22:16:34 +0700

    {"types":"C","runs":394}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -744,7 +744,7 @@ public class GraphicsContest extends GraphicsProgram {
 				reflectionArray[1][1] = -A;
 				coordinate[2][0] = -x;
 				coordinate[0][2] = y;
-				for (int i = 2; i <= (int) Math.pow(2, n); i++) {
+				for (int i = 1; i <= (int) Math.pow(2, n); i++) {
 					coordinate[(int) (Math.pow(2, n) + i)][0] = coordinate[i][0]*(reflectionArray[0][0]) + coordinate[0][i]*(reflectionArray[0][1]);
 					coordinate[0][(int) (Math.pow(2, n) + i)] = coordinate[i][0]*(reflectionArray[1][0]) + coordinate[0][i]*(reflectionArray[1][1]);
 				}
