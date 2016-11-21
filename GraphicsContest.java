712615ec1fb0029c7f34feee75bd9a1e557bf1c2
S <s>
2016-11-21 07:40:29 +0700

    {"types":"C","runs":445}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -20,7 +20,7 @@ public class GraphicsContest extends GraphicsProgram {
 	private static final int WIDTH = 700;
 	private static final double S_ICON_WIDTH = WIDTH/20;
 	private static final double M_ICON_WIDTH = WIDTH/10;
-	private static final double L_ICON_WIDTH = 2.5*WIDTH/20;
+	private static final double L_ICON_WIDTH = WIDTH/20;
 	private static final int HEIGHT = 700 + ICON_HEIGHT;
 	private static final int PAUSE = 10;
 	private static final int BRUSH_SIZE = 5;
