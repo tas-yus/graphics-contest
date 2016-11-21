6b67dfe08042dfb982cefa5f39cf1bfe619c0070
S <s>
2016-11-21 15:42:20 +0700

    {"types":"C","runs":532}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -218,7 +218,8 @@ public class GraphicsContest extends GraphicsProgram {
 		add(colorIcon8);
 		colorIcon9 = new GRect (colorIcon8.getX() + colorIcon8.getWidth(), 0, width, ICON_HEIGHT);
 		add(colorIcon9);
-		setUpLines(plane);
+	
+		
 		coordinate = new double[(int) Math.pow(2, (plane)) + 1][(int) Math.pow(2, (plane)) + 1];
 	}
 
@@ -250,12 +251,7 @@ public class GraphicsContest extends GraphicsProgram {
 		remove(symmetryNum);
 		remove(planeNum);
 		removeSymLine(symLine);
-		if (rotation == true) {
-			setUpLines(plane);
-		}
-		if (reflection == true) {
-			setUpLines(symmetry);
-		}
+		setUpLines(plane);
 		coordinate = new double[(int) Math.pow(2, (plane)) + 1][(int) Math.pow(2, (plane)) + 1];
 		brushStatus = new GLabel ("Brush: " + status, icon1.getX() + icon1.getWidth()/2, ICON_HEIGHT/2);
 		brushStatus.move(-brushStatus.getWidth()/2, +brushStatus.getAscent()/2);
