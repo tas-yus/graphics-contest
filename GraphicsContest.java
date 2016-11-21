3b6e9b3055db444a9ca00a4ddfd0728a3097b1d5
S <s>
2016-11-21 15:52:42 +0700

    {"types":"C","runs":540}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -218,8 +218,7 @@ public class GraphicsContest extends GraphicsProgram {
 		add(colorIcon8);
 		colorIcon9 = new GRect (colorIcon8.getX() + colorIcon8.getWidth(), 0, width, ICON_HEIGHT);
 		add(colorIcon9);
-		if (rotation == true) setUpLines(symmetry);
-		if (reflection == true) setUpLines(plane);
+		setUpLines(plane);
 		coordinate = new double[(int) Math.pow(2, (plane)) + 1][(int) Math.pow(2, (plane)) + 1];
 	}
 
