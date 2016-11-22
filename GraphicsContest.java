43f54a932e414ab0cecdcfea564d6e317c9d672b
S <s>
2016-11-22 07:36:52 +0700

    {"types":"C","runs":36}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -434,8 +434,7 @@ public class GraphicsContest extends GraphicsProgram {
 		if (clickIcon9(e) == true) {
 			BufferedImage bi = new BufferedImage(this.getSize().width, this.getSize().height, BufferedImage.TYPE_INT_ARGB); 
 			Graphics g = bi.createGraphics();
-			this.paint(g); 
-			g.dispose();
+			this.print(getGraphics());
 			try {
 			ImageIO.write(bi,"png",new File("test.png"));
 			}
