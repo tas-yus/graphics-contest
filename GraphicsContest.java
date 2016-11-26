420161c583ae8736c66e5c2085e354567bf22572
S <s>
2016-11-26 10:28:13 +0700

    {"types":"C","runs":519}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -901,7 +901,7 @@ public class GraphicsContest extends GraphicsProgram {
 					coordinate[0][(int) (Math.pow(2, n) + i)] = coordinate[i][0]*(reflectionArray[1][0]) + coordinate[0][i]*(reflectionArray[1][1]);
 				}
 			}
-			double N = Math.log10(2*fold)/Math.log(2) + 1;
+			double N = Math.log10(2*fold)/Math.log10(2) + 1;
 			for (int j = 1; j < N; j++) {
 				double X = coordinate[j][0];
 				double Y = coordinate[0][j];
